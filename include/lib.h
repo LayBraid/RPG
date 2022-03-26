75200f75ed79528ec1666e7796662033cb63b234
Arthur Pahon <arthur.pahon@epitech.eu>
2022-03-26 17:11:32 +0100

    include strdup

--- include/lib.h
+++ include/lib.h
@@ -8,10 +8,11 @@
 #ifndef LIB_H_
     #define LIB_H_
 
-void my_putchar(char c);
-int my_putnbr(int nb);
-int my_getnbr(char *str);
-int my_putstr(char const *str);
-int my_strlen(char const *str);
+    void my_putchar(char c);
+    int my_putnbr(int nb);
+    int my_getnbr(char *str);
+    int my_putstr(char const *str);
+    int my_strlen(char const *str);
+    char *my_strdup(char *str);
 
 #endif /* !LIB_H_ */
