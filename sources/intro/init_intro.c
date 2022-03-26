0aad6b49b34dd35cfe28b055e20cb586c9bfd4c3
CHAVEROT Aymeric <aymeric.chaverot@gmail.com>
2022-03-26 17:31:14 +0100

    fixed things for initial display

--- sources/intro/init_intro.c
+++ sources/intro/init_intro.c
@@ -9,12 +9,12 @@
 
 void init_intro(data_t *data)
 {
-    printf("teszez igiea\n");
-    create_texture(data->texture_bank,
+    data->texture_bank = create_texture(data->texture_bank,
     "assets/intro_menu/metro.png", &(sfIntRect){16, 400, 256, 145});
     //set_texture_rect(data->texture_bank, (sfIntRect);
-    create_tile(data->tiles);
-    set_tile_texture(data->tiles, data->texture_bank);
+    data->tiles = create_tile(data->tiles);
+    printf("teszez igiea %p\n", data->texture_bank);
+    data->tiles = set_tile_texture(data->tiles, data->texture_bank);
     // create_texture(data->texture_bank,
     // "../../assets/intro_menu/metro.png", &(sfIntRect){16, 400, 0, 0});
     //set_texture_rect(data->texture_bank, (sfIntRect){0, 0, 0, 0}, 0);
--- sources/structs/tiles_utils.c
+++ sources/structs/tiles_utils.c
@@ -9,9 +9,9 @@
 
 tile_t *set_tile_texture(tile_t *node, texture_t *texture)
 {
-    node->rect = texture->rect;
+    //node->rect = texture->rect;
     sfSprite_setTexture(node->sprite, texture->texture, sfTrue);
-    sfSprite_setTextureRect(node->sprite, node->rect);
+    //sfSprite_setTextureRect(node->sprite, node->rect);
     return (node);
 }
 
