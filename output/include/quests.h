2443e5adb9102491d525627e6a2e9f6d716f7ef3
LayBraid <clement.loeuillet@epitech.eu>
2022-04-11 23:12:17 +0200

    :white_check_mark: add function to affect quest when an event is calling

--- output/include/quests.h
+++ output/include/quests.h
@@ -10,10 +10,21 @@
 
     #include "structures.h"
 
+enum STATUS {
+    NO_SEE,
+    IN_PROGRESS,
+    CLAIM,
+    FINISH,
+};
+
 void add_quest(data_t *data, quest_t *quest);
 
 void any_quest(quest_t **node, quest_t *new);
 
 void first_quest(quest_t **node, quest_t *new);
+
+void call_to_quests(data_t *data, char *event);
+
+void finish_quest(data_t *data, quest_t *quest);
 
 #endif
---
+++ output/sources/quests/events.c
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2022
+** output
+** File description:
+** No file there , just an epitech header example
+*/
+
+#include "quests.h"
+#include "my.h"
+
+static void update_quest(quest_t *quest)
+{
+    event_list_t *tmp = quest->requirements;
+
+    while (tmp->next != NULL) {
+        if (tmp->calls > 0)
+            return;
+        tmp = tmp->next;
+    }
+    if (tmp->calls > 0)
+        return;
+    quest->status = CLAIM;
+}
+
+static void call_to_requirements(quest_t *quest, char *event)
+{
+    event_list_t *tmp = quest->requirements;
+
+    while (tmp->next != NULL) {
+        if (my_strcmp(tmp->name, event) == 0 && tmp->calls > 0) {
+            tmp->calls--;
+            update_quest(quest);
+        }
+        tmp = tmp->next;
+    }
+    if (my_strcmp(tmp->name, event) == 0 && tmp->calls > 0) {
+        tmp->calls--;
+        update_quest(quest);
+    }
+}
+
+void call_to_quests(data_t *data, char *event)
+{
+    quest_t *tmp = data->quest;
+
+    while (tmp->next != NULL) {
+        call_to_requirements(tmp, event);
+        tmp = tmp->next;
+    }
+    call_to_requirements(tmp, event);
+}
