59a05bec3495d1f6f52d2f04be7b659c916d8a06
AymericChaverot <aymeric.chaverot@gmail.com>
2022-04-12 14:20:01 +0200

    finished merge

--- output/sources/structs/tiles_utils.c
+++ output/sources/structs/tiles_utils.c
@@ -41,7 +41,6 @@ tile_t *set_tile_move(tile_t *node, sfVector2f move)
     sfSprite_move(node->sprite, node->move);
     return (node);
 }
-<<<<<<< HEAD:output/sources/structs/tiles_utils.c
 
 tile_t *set_tile_rotation(tile_t *node, float rotation)
 {
@@ -49,5 +48,3 @@ tile_t *set_tile_rotation(tile_t *node, float rotation)
     sfSprite_setRotation(node->sprite, node->rotation);
     return (node);
 }
-=======
->>>>>>> abd0ecb38f739064b5618a33edfce34019d6bf10:sources/structs/tiles_utils.c
