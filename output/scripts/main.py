62b751b00c98b73070361b231cba745e8ee057bf
LayBraid <clement.loeuillet@epitech.eu>
2022-04-20 14:50:32 +0200

    :bug: fix scripts for rects

--- output/scripts/main.py
+++ output/scripts/main.py
@@ -8,16 +8,16 @@ rect_height = sys.argv[5]
 type = sys.argv[6]
 
 t = open("../sources/editor/textures/texture_1.c", "a")
-t.write("\nvoid set_" + str(function_name) + "(editor_t *editor, node_rectangle *rectangle)\n{\n    sfIntRect rect = {" +
+t.write("\nvoid set_" + str(function_name) + "(sfTexture *texture, node_rectangle *rectangle)\n{\n    sfIntRect rect = {" +
         str(rect_left) + "," + str(rect_top) + "," + str(rect_width) + "," + str(
     rect_height) + "};\n    rectangle->type = "
-        + str(type) + ";\n    sfRectangleShape_setTexture(rectangle->rectangle, editor->world, sfTrue);\n"
+        + str(type) + ";\n    sfRectangleShape_setTexture(rectangle->rectangle, texture, sfTrue);\n"
         + "    sfRectangleShape_setTextureRect(rectangle->rectangle, rect);\n}\n")
 t.close()
 
 
 h = open("../include/editor.h", "a")
-h.write("\nvoid set_" + str(function_name) + "(editor_t *editor, node_rectangle *rectangle);")
+h.write("\nvoid set_" + str(function_name) + "(sfTexture *texture, node_rectangle *rectangle);")
 h.close()
 
 i = open("../sources/editor/textures/initializer.c", "a")
