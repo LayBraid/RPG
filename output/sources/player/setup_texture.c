5fadb4a586a0c983460de4bed9cb645b81592ef2
LayBraid <clement.loeuillet@epitech.eu>
2022-04-22 17:35:30 +0200

    :recycle: update branch

--- output/sources/player/setup_texture.c
+++ output/sources/player/setup_texture.c
@@ -13,11 +13,11 @@ void setup_texture_player(data_t *data)
     data->player.state = NOTHING;
     data->player.movement = NULL;
     data->player.movement_anim = NULL;
-    sfRectangleShape_setSize(data->player.rectangle, (sfVector2f) {64, 100});
+    sfRectangleShape_setSize(data->player.rectangle, (sfVector2f) {16, 28});
     sfRectangleShape_setTexture(data->player.rectangle,
-    sfTexture_createFromFile("assets/game/Link.png", NULL), sfTrue);
+    sfTexture_createFromFile("assets/game/LinkBorder.png", NULL), sfTrue);
     sfRectangleShape_setTextureRect(data->player.rectangle,
     get_rect_player(data->player.state, data->player.animation));
     sfRectangleShape_setPosition(data->player.rectangle, (sfVector2f)
-    {0, 980});
+    {0, 1500 - 26});
 }
