6669f0494b7448f4d62508c38d2ffe2243b9a60f
LayBraid <clement.loeuillet@epitech.eu>
2022-04-26 22:04:52 +0200

    :construction: update display of npc

--- output/sources/display/display_all.c
+++ output/sources/display/display_all.c
@@ -11,15 +11,15 @@
 void display_all(data_t *data)
 {
     sfRenderWindow_setView(data->video.window, data->main);
-    for (unsigned char depth = 0; depth < 10; depth++) {
+    for (unsigned char depth = 0; depth < 10; depth++)
         display_tile_depth(data->tiles, data->video, depth);
-        display_npc_depth(data->npcs, data->video, depth);
-    }
     display_buttons(data->buttons, data->video);
     display_texts(data->texts, data->video);
     if (data->video.ui == 2) {
         sfRenderWindow_setView(data->video.window, data->mapping);
         display_rectangles_map(data);
         display_player_depth(data->player, data->video, data->player.depth);
+        for (unsigned char depth = 0; depth < 10; depth++)
+            display_npc_depth(data->npcs, data->video, depth);
     }
 }
