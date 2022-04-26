bc2ce9b1e1093ab5a433f2409b54b370e189e7f8
AymericChaverot <aymeric.chaverot@gmail.com>
2022-04-26 15:59:59 +0200

    added slow effect for more than one '.' at a time and added and placed correctly involved character names

--- output/sources/dialogs/dialogs.c
+++ output/sources/dialogs/dialogs.c
@@ -36,7 +36,7 @@ static void set_dialog_characters(data_t *data, int id_npc_texture, char is_talk
     texture_t *player = data->texture_bank;
     texture_t *npc_texture = data->texture_bank;
     npc_t *npc = data->npcs;
-
+/*
     while (player != NULL && player->id != data->id_text_player)
         player = player->next;
     while (npc_texture != NULL && npc_texture->id != id_npc_texture)
@@ -54,26 +54,28 @@ static void set_dialog_characters(data_t *data, int id_npc_texture, char is_talk
     if (is_talking)
         data->tiles = set_tile_depth(data->tiles, 6);
     else
-        data->tiles = set_tile_depth(data->tiles, 7);
+        data->tiles = set_tile_depth(data->tiles, 7); */
     data->texts = create_text(data->texts, data->player.name, data->font);
     data->texts = create_text(data->texts, npc->name, data->font);
     if (is_talking)
         sfText_setStyle(data->texts->text, sfTextBold);
     else
         sfText_setStyle(data->texts->next->text, sfTextBold);
-    data->texts = set_text_position(data->texts, (sfVector2f){500, 500});
-    data->texts->next = set_text_position(data->texts->next, (sfVector2f){500, 550});
+    sfText_setPosition(data->texts->text, (sfVector2f){15, 875});
+    sfText_setPosition(data->texts->next->text, (sfVector2f){1750, 875});
+    // data->texts = set_text_position(data->texts, (sfVector2f){500, 500});
+    // data->texts->next = set_text_position(data->texts->next, (sfVector2f){500, 550});
 }
 
-static void dialog(data_t *data, char *dialog, int id_npc, char is_talking)
+static void dialog(data_t *data, char *dialog, int id_npc_texture, char is_talking, int id_npc)
 {
     char *new = malloc((strlen(dialog) + 1) * sizeof(char));
 
     new[0] = '\0';
+    set_dialog_characters(data, id_npc_texture, is_talking, id_npc); //TODO tester situation reelle
     data->texts = create_text(data->texts, new, data->font);
     sfText_setColor(data->texts->text, sfWhite);
     sfText_setPosition(data->texts->text, (sfVector2f){100, 900});
-    //set_dialog_characters(data, id_npc, is_talking); //TODO tester situation reelle
     for (int i = 0; dialog[i]; i++) {
         new[i] = dialog[i];
         new[i + 1] = '\0';
@@ -83,8 +85,12 @@ static void dialog(data_t *data, char *dialog, int id_npc, char is_talking)
         }
         sfText_setString(data->texts->text, new);
         printf("%d\n", i);
-        if (dialog[i] != ' ' && data->dialog_skip == 0)
-            while (sfClock_getElapsedTime(data->clock).microseconds < 50000);
+        if (dialog[i] != ' ' && data->dialog_skip == 0) {
+            if (dialog[i] == '.' && dialog[i - 1] == '.')
+                while (sfClock_getElapsedTime(data->clock).microseconds < 600000);
+            else
+                while (sfClock_getElapsedTime(data->clock).microseconds < 50000);
+        }
         analyse_events(data);
         sfRenderWindow_clear(data->video.window, sfWhite);
         display_all(data);
@@ -150,8 +156,11 @@ void destroy_dialogs(data_t *data)
 
 void dialog_init(data_t *data)
 {
+    data->player.name = strdup("Link");
+    data->npcs = create_npc(data->npcs, "Kamel");
     data->tiles = create_tile(data->tiles);
     data->texture_bank = create_texture(data->texture_bank, "./assets/textures/dialog.png", NULL);
+    data->texture_bank = create
     data->tiles = set_tile_texture(data->tiles, data->texture_bank);
     data->tiles = set_tile_depth(data->tiles, 6);
     data->tiles = set_tile_scale(data->tiles, (sfVector2f) {4, 4});
@@ -162,17 +171,36 @@ void dialog_init(data_t *data)
     data->tiles = set_tile_position(data->tiles, (sfVector2f) {0, 870});
     data->dialog_skip = 0;
     printf("here\n");
-    dialog(data, "The Legend of Zelda inspired game, by : not Oscar who's not here.ABCDEFGHIJKLMNOPQ\n\nBite\n\nEddie fais la map stp wllh c'est relou mon reuf c'est long sa mere.", 0, 0);
+    dialog(data, "...\n\n...........\n\n#choque", 0, 0, 1);
     printf("entering dialog_inter\n");
     inter_dialog(data);
+    sfText_destroy(data->texts->next->next->text);
+    free(data->texts->next->next);
+    data->texts->next->next = NULL;
+    sfText_destroy(data->texts->next->text);
+    free(data->texts->next);
+    data->texts->next = NULL;
     sfText_destroy(data->texts->text);
     free(data->texts);
     data->texts = NULL;
-    dialog(data, "This is a second dialog\n\nHehe you cringe\n\ncringos", 0, 0);
+    dialog(data, "Ola ! Je suis Kamel, le CEO de KFC !\n\nAlors comme ca on a mange des donuts sucres ? Au sucre ??\n\nBon garcon...", 0, 1, 1);
+    // delete_all_texts(data->texts);
     inter_dialog(data);
+    sfText_destroy(data->texts->next->next->text);
+    free(data->texts->next->next);
+    data->texts->next->next = NULL;
+    sfText_destroy(data->texts->next->text);
+    free(data->texts->next);
+    data->texts->next = NULL;
     sfText_destroy(data->texts->text);
     free(data->texts);
     data->texts = NULL;
+    // sfText_destroy(data->texts->text);
+    // free(data->texts);
+    // data->texts = NULL;
+    data->npcs = NULL;
+    // delete_all_npcs(data->npcs);
+    // free(data->player.name);
     // delete_first_tile(data);
     destroy_dialogs(data);
 }
