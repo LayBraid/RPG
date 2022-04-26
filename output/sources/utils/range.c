5e908670ca3d455882c3f58cc63f751615f36f5a
LayBraid <clement.loeuillet@epitech.eu>
2022-04-26 22:19:04 +0200

    :white_check_mark: add my_sqrt

--- output/sources/utils/range.c
+++ output/sources/utils/range.c
@@ -8,6 +8,25 @@
 #include "utils.h"
 #include "math.h"
 
+double my_sqrt(double nb)
+{
+    double result = 0;
+    double i = 0;
+
+    while (i * i <= nb) {
+        i += 0.1;
+    }
+    i -= 0.1;
+    while (i * i <= nb) {
+        if (i * i == nb) {
+            result = i;
+            return (result);
+        }
+        i += 0.1;
+    }
+    return (result);
+}
+
 int my_range(sfVector2i v1, sfVector2i v2, double range)
 {
     int x = v2.x - v1.x;
