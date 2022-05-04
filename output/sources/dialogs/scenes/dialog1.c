56f03aedc2d910c8b0945eb33e186c1e8e9ac6e0
Arthur Pahon <arthur.pahon@epitech.eu>
2022-05-04 16:48:39 +0200

    fonction test

--- output/sources/dialogs/scenes/dialog1.c
+++ output/sources/dialogs/scenes/dialog1.c
@@ -8,3 +8,19 @@
 #include "dialogs.h"
 #include "my_rpg.h"
 
+void dialog1(data_t *data, int npc_id)
+{
+    dialog_init(data);
+    dialog(data,
+        "Ceci est un dialogue\n\npour montrer\n\nle fonctionnement.",
+        data->id_text_player, 0, npc_id);
+    inter_dialog(data);
+    destroy_dialog_frame(data);
+    dialog(data,
+        "Ola ! Je suis Arthur !\n\nJ'ai fait les menus du jeu\n\nLa suite...",
+        data->id_text_player, 1, npc_id);
+    inter_dialog(data);
+    destroy_dialog_frame(data);
+    destroy_dialogs(data);
+    data->video.ui = 6;
+}
