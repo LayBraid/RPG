6bdaa8dbaf1c1c6abce46531459dd2580978f626
LayBraid <clement.loeuillet@epitech.eu>
2022-05-04 17:27:14 +0200

    :bug: update delete attack effect

--- output/sources/enemies/clock_effect.c
+++ output/sources/enemies/clock_effect.c
@@ -10,8 +10,6 @@
 static void clock_effect(enemy_t *enemy, attack_effect_t *node)
 {
     if (node->movement == NULL)
-        enemy->attack_effect = enemy->attack_effect->next;
-    if (node->movement == NULL)
         return;
     sfTime time = sfClock_getElapsedTime(node->movement_clock);
     double diff = time.microseconds / 1000000.0;
@@ -19,6 +17,8 @@ static void clock_effect(enemy_t *enemy, attack_effect_t *node)
     if (diff > 0.06) {
         sfRectangleShape_setPosition(node->rectangle, node->movement->delta);
         node->position = sfRectangleShape_getPosition(node->rectangle);
+        if (node->movement->next == NULL)
+            enemy->attack_effect = enemy->attack_effect->next;
         node->movement = node->movement->next;
         sfClock_restart(node->movement_clock);
     }
