71830a5604595178181de69e51fae8f845ef687f
Eddieklai <eddie.klai@epitech.eu>
2022-05-04 20:17:09 +0200

    3 sword

--- output/sources/my_event/functions/player/clock_sword.c
+++ output/sources/my_event/functions/player/clock_sword.c
@@ -14,10 +14,8 @@ static void clock_effect(player_t *player, attack_effect_t *node)
     sfTime time = sfClock_getElapsedTime(node->movement_clock);
     double diff = time.microseconds / 1000000.0;
 
-    if (diff > 0.1) {
-        printf("up anim %d\n%d %d\n\n", node->animation->value, player->attack_effect->type, node->animation->value);
+    if (diff > 0.02) {
         sfVector2f fdp = sfRectangleShape_getPosition(node->rectangle);
-        printf("%f %f\n", fdp.x, fdp.y);
         sfRectangleShape_setSize(node->rectangle, get_size_sword(player->attack_effect->type, node->animation->value));
         sfRectangleShape_setTextureRect(node->rectangle, get_rect_sword(player->attack_effect->type, node->animation->value));
         if (player->attack_effect->animation->next == NULL) {
