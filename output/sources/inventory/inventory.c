2aeb058c0f7e74bc2ad232f83340458cf5e9dab6
Eddieklai <eddie.klai@epitech.eu>
2022-05-06 18:23:00 +0200

    fix compilation

--- output/sources/inventory/inventory.c
+++ output/sources/inventory/inventory.c
@@ -16,7 +16,7 @@ void get_inventory_mouse_click(data_t *data)
     while (tmp != NULL) {
         pos = (sfIntRect){tmp->position_rect.x, tmp->position_rect.y, 125, 125};
         if (is_in_rect(tmp->rect_rect, data->event.mouseButton.x, data->event.mouseButton.y) == 0) {
-            data->player.equiped = tmp->item;
+            data->player.equipped = tmp->item;
             data->video.ui = 2;
         }
         tmp = tmp->next;
