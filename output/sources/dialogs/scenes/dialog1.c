99a300fa2641821c066c781661e109b072797c95
AymericChaverot <aymeric.chaverot@gmail.com>
2022-05-06 16:10:41 +0200

    клемент тебя хорошо ебет мой парень уже что ты ничего не дрочишь и что ты только поешь не начинай оскорблять нас в коммиты большая сволочь

--- output/sources/dialogs/scenes/dialog1.c
+++ output/sources/dialogs/scenes/dialog1.c
@@ -9,30 +9,6 @@
 #include "dialogs.h"
 #include "my_fprintf.h"
 
-// void dialog1(data_t *data, int npc_id)
-// {
-//     dialog_init(data);
-//     printf("dialog 1\n");
-//     dialog(data,
-//         "Ceci est un dialogue\n\npour montrer\n\nle fonctionnement.",
-//         data->id_text_player, 0, npc_id);
-//     printf("inter 1\n");
-//     inter_dialog(data);
-//     printf("destroy 1\n");
-//     destroy_dialog_frame(data);
-//     printf("dialog 2\n");
-//     dialog(data,
-//         "Ola ! Je suis Arthur !\n\nJ'ai fait les menus du jeu\n\nLa suite...",
-//         data->id_text_player, 1, npc_id);
-//     printf("inter 2\n");
-//     inter_dialog(data);
-//     printf("destroy 2\n");
-//     destroy_dialog_frame(data);
-//     printf("destroy 3\n");
-//     destroy_dialogs(data);
-//     data->video.ui = 2;
-// }
-
 // ! destroy dialog frame bug (segfault)
 
 void dialog1(data_t *data, int npc_id)
