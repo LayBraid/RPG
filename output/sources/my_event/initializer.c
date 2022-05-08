33c653f00c80348069a68c84acf8247f5fe65ffd
Eddieklai <eddie.klai@epitech.eu>
2022-05-08 16:29:40 +0200

    fix anim sword and range

--- output/sources/my_event/initializer.c
+++ output/sources/my_event/initializer.c
@@ -29,4 +29,6 @@ void initialize_events(data_t *data)
     add_event(&data->my_event, "enemy_move", enemy_move);
     add_event(&data->my_event, "enemy_attack", enemy_attack);
     add_event(&data->my_event, "loose", loose_game);
+    add_event(&data->my_event, "attack_player", attack_player);
+    add_event(&data->my_event, "attack_on_enemy", attack_on_enemy);
 }
